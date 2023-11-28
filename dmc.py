6eef8120d4b4b14c16ebeb512e061ae3bc2ccdcc
Millz98 <dave_m86@hotmail.com>
2023-11-28 16:24:26 -0600

    Fixed the audio download issue

--- dmc.py
+++ dmc.py
@@ -2,6 +2,9 @@ from pytube import YouTube, Playlist
 from tqdm import tqdm
 import requests
 import os
+from moviepy.editor import AudioFileClip
+
+os.environ["IMAGEIO_FFMPEG_EXE"] = "/opt/homebrew/opt/ffmpeg"
 
 def print_video_information(yt):
     print(f"Title: {yt.title}")
@@ -24,10 +27,17 @@ def download_audio(yt, destination):
                         f.write(chunk)
                         bar.update(1024)
 
-    # save the audio file as MP3
+    # save the audio file as MP3 using moviepy
     base, _ = os.path.splitext(os.path.join(destination, audio_stream.default_filename))
     audio_new_file = base + '.mp3'
-    os.rename(os.path.join(destination, audio_stream.default_filename), audio_new_file)
+    
+    # Convert audio file to MP3 using MoviePy
+    audio_clip = AudioFileClip(os.path.join(destination, audio_stream.default_filename))
+    audio_clip.write_audiofile(audio_new_file, codec='mp3')
+    audio_clip.close()
+
+    # remove the original audio file
+    os.remove(os.path.join(destination, audio_stream.default_filename))
 
 def download_video_file(yt, destination):
     # get the best quality stream for video
@@ -65,7 +75,7 @@ def download_playlist(playlist_url, choice, destination):
         for video_url in playlist.video_urls:
             try:
                 yt = YouTube(video_url)
-                download_video(yt, choice, destination)
+                download_audio(yt, destination)
             except Exception as e:
                 print(f"Error processing video {video_url}: {str(e)}")
 
@@ -76,28 +86,28 @@ def download_playlist(playlist_url, choice, destination):
 # Example usage
 choice = int(input("Choose download format:\n1. MP3 (Audio)\n2. MP4 (Video)\nEnter your choice (1 or 2): "))
 
-if choice == 2:
-    download_option = int(input("Choose download option:\n1. Single Video\n2. Entire Playlist\nEnter your choice (1 or 2): "))
-    
-    if download_option == 1:
-        # Download a single video
-        video_url = input("Enter the URL of the YouTube video you want to download: ")
-        destination = input("Enter the full path to the destination folder (e.g., D:\\Downloads): ")
-        
-        try:
-            yt = YouTube(video_url)
-            download_video(yt, choice, destination)
-        except Exception as e:
-            print(f"Error processing video {video_url}: {str(e)}")
-        
-    elif download_option == 2:
-        # Download an entire playlist
-        playlist_url = input("Enter the URL of the YouTube playlist you want to download: ")
-        destination = input("Enter the full path to the destination folder (e.g., D:\\Downloads): ")
-        
-        download_playlist(playlist_url, choice, destination)
+download_option = int(input("Choose download option:\n1. Single Video\n2. Entire Playlist\nEnter your choice (1 or 2): "))
 
-    else:
-        print("Invalid download option.")
-else:
+if download_option == 1:
+    # Download a single video
+    video_url = input("Enter the URL of the YouTube video you want to download: ")
+    destination = input("Enter the full path to the destination folder (e.g., D:\\Downloads): ")
+
+    try:
+        yt = YouTube(video_url)
+        download_video(yt, choice, destination)
+    except Exception as e:
+        print(f"Error processing video {video_url}: {str(e)}")
+
+elif download_option == 2 and choice == 2:
+    # Download an entire playlist for video
+    playlist_url = input("Enter the URL of the YouTube playlist you want to download: ")
+    destination = input("Enter the full path to the destination folder (e.g., D:\\Downloads): ")
+
+    download_playlist(playlist_url, choice, destination)
+
+elif download_option == 2 and choice == 1:
     print("Downloading audio does not support playlist downloads.")
+
+else:
+    print("Invalid download option.")
