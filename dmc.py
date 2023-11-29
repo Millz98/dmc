2bc3f7f434119cf968bb6901fc50322c951c7afd
Millz98 <dave_m86@hotmail.com>
2023-11-29 17:04:53 -0600

    GUI added

--- dmc.py
+++ dmc.py
@@ -3,6 +3,71 @@ from tqdm import tqdm
 import requests
 import os
 from moviepy.editor import AudioFileClip
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit, QFileDialog, QRadioButton
+
+class DMCApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        # Widgets
+        self.label_url = QLabel("Enter the URL of the YouTube video or playlist you want to download:")
+        self.url_input = QLineEdit(self)
+        self.destination_input = QLineEdit(self)
+        self.destination_input.setPlaceholderText("Enter the destination folder path")
+        self.browse_button = QPushButton("Browse", self)
+        self.browse_button.clicked.connect(self.browse_destination)
+
+        self.radio_audio = QRadioButton("MP3 (Audio)")
+        self.radio_video = QRadioButton("MP4 (Video)")
+        
+        self.download_button = QPushButton("Download", self)
+        self.download_button.clicked.connect(self.download)
+
+        # Layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.label_url)
+        layout.addWidget(self.url_input)
+        layout.addWidget(self.destination_input)
+        layout.addWidget(self.browse_button)
+        layout.addWidget(self.radio_audio)
+        layout.addWidget(self.radio_video)
+        layout.addWidget(self.download_button)
+
+        self.setLayout(layout)
+
+    def browse_destination(self):
+        folder_path = QFileDialog.getExistingDirectory(self, "Select Destination Folder")
+        self.destination_input.setText(folder_path)
+
+    def download(self):
+        url = self.url_input.text()
+        destination = self.destination_input.text()
+
+        if not url or not destination:
+            print("Please enter both URL and destination folder.")
+            return
+
+        try:
+            yt = YouTube(url)
+
+            # Print video information
+            print_video_information(yt)
+
+            # Download the chosen file
+            choice = 1 if self.radio_audio.isChecked() else 2
+
+            if "/playlist" in url:
+                download_playlist(url, choice, destination)
+            else:
+                download_video(yt, choice, destination)
+
+            # Result of success
+            print(f"{yt.title} has been successfully downloaded.")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
 
 def print_video_information(yt):
     print(f"Title: {yt.title}")
@@ -82,44 +147,10 @@ def download_playlist(playlist_url, choice, destination):
         print(f"An error occurred while processing the playlist: {str(e)}")
 
 def main():
-    while True:
-        print("\nOptions:")
-        print("1. Download")
-        print("2. Quit")
-
-        option = input("Enter your choice (1 or 2): ")
-
-        if option == "1":
-            choice = int(input("Choose download format:\n1. MP3 (Audio)\n2. MP4 (Video)\nEnter your choice (1 or 2): "))
-            download_option = int(input("Choose download option:\n1. Single Video\n2. Entire Playlist\nEnter your choice (1 or 2): "))
-
-            if download_option == 1:
-                # Download a single video
-                video_url = input("Enter the URL of the YouTube video you want to download: ")
-                destination = input("Enter the full path to the destination folder (e.g., D:\\Downloads): ")
-                
-                try:
-                    yt = YouTube(video_url)
-                    download_video(yt, choice, destination)
-                except Exception as e:
-                    print(f"Error processing video {video_url}: {str(e)}")
-                
-            elif download_option == 2:
-                # Download an entire playlist
-                playlist_url = input("Enter the URL of the YouTube playlist you want to download: ")
-                destination = input("Enter the full path to the destination folder (e.g., D:\\Downloads): ")
-                
-                download_playlist(playlist_url, choice, destination)
-
-            else:
-                print("Invalid download option.")
-
-        elif option == "2":
-            print("Thank you for using the DMC!!")
-            break
-
-        else:
-            print("Invalid option. Please enter 1 or 2.")
+    app = QApplication([])
+    window = DMCApp()
+    window.show()
+    app.exec_()
 
 if __name__ == "__main__":
     main()
